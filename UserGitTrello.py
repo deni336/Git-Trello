5f0e646b5855b63b4643b76a1db315c5ba1809f5
deni336 <33584555+deni336@users.noreply.github.com>
2022-03-22 20:20:18 -0600

    Completed Script
    Take the title and description of a trello card and insert into html doc, then upload to github.

--- UserGitTrello.py
+++ UserGitTrello.py
@@ -1,6 +1,6 @@
+import re
 from trello import TrelloClient
 from github import Github
-from html.parser import HTMLParser
 import time
 
 client = TrelloClient(api_key='4cd5173d5fbf8dceb732ac54a410e406',
@@ -17,7 +17,7 @@ def getTrelloCard():
 
 def recurringLoop(cardProfile):
     n = 5
-    oldTrelloCard = []
+    oldTrelloCard = getTrelloCard
     while n > 0:
         newTrelloCard = getTrelloCard()
         if newTrelloCard != oldTrelloCard:
@@ -26,7 +26,7 @@ def recurringLoop(cardProfile):
         else:
             time.sleep(30)
 
-header = '''</div><!DOCTYPE html>
+header = '''<!DOCTYPE html>
     <html>
     <head>
         <meta charset="utf-8">
@@ -38,7 +38,8 @@ header = '''</div><!DOCTYPE html>
     <body>
         <div class="main">
             <div class="header">
-                <h1 id="title">ViperVision Release Notes</h1>\n'''
+                <h1 id="title">ViperVision Release Notes</h1></div>'''
+
 def stringParser(newTrelloCard):
     versionTitle = newTrelloCard[0]
     versionDescription = newTrelloCard[1]
@@ -46,64 +47,39 @@ def stringParser(newTrelloCard):
     completedString = ''
     with open("index.html", "r") as od:
         oldData = od.read()
-    checkVersion = oldData.split('div')
-    checkVersion = checkVersion[5].split('"')
-    if checkVersion[1] != versionTitle:
+    if oldData.find(versionTitle) == False:
         for string in splitString:
             if string.endswith('.') == True:
-                line = "<li>" + string + "</li>\n"
+                line = "<li>" + string + "</li>"
                 completedString = completedString + line
+            elif string.isspace() == True:
+                pass
             else:
-                head = "<h3>" + string + "</h3>\n"
+                head = "<h3>" + string + "</h3>"
                 completedString = completedString + head
-        completedOutput = f'''<div id={versionTitle}><div class="align"><i id="i34" class="fa fa-minus" onclick="makeLittle('collapse-34', 'i34')" aria-hidden="true"></i><h2>ViperVision4.7.13.19</h2></div><ul id="collapse-34" class="active" style="display: block;">\n {completedString}</div>'''
-        oldData = oldData.replace('''</div><!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="index.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="icon" href="ViperWhite.ico">
-        <title>ViperVisionReleaseNotes</title>
-    </head>
-    <body>
-        <div class="main">
-            <div class="header">
-                <h1 id="title">ViperVision Release Notes</h1>\n''', "")
+        completedOutput = f'''<div id={versionTitle}><div class="align"><i id="i34" class="fa fa-minus" onclick="makeLittle('collapse-34', 'i34')" aria-hidden="true"></i><h2>ViperVision4.7.13.19</h2></div><ul id="collapse-34" class="active" style="display: block;"> {completedString}</ul></div>'''
         oldData = oldData.replace('''style="display: block;"''', "")
         oldData = oldData.replace("minus", "plus")
-        
         finishedOutput = header + completedOutput + oldData
         file = open("index.html", "w")
         file.write(finishedOutput)
         file.close
     else:
-        splitOldData = oldData.split("</div")
-        removedData1 = splitOldData.pop(1)
-        removedData2 = splitOldData.pop(1)
+        splitOldData = re.split("(\</div>)", oldData)
+        for i in range(0,6):    
+            splitOldData.pop(0)
         newOldData = ""
         oldDataWithoutPreviousVersion = newOldData.join(splitOldData)
         for string in splitString:
             if string.endswith('.') == True:
                 line = "<li>" + string + "</li>\n"
                 completedString = completedString + line
+            elif string == "":
+                pass
             else:
                 head = "<h3>" + string + "</h3>\n"
                 completedString = completedString + head
-        completedOutput = f'''<div id="{versionTitle}"><div class="align"><i id="i34" class="fa fa-minus" onclick="makeLittle('collapse-34', 'i34')" aria-hidden="true"></i><h2>ViperVision4.7.13.19</h2></div><ul id="collapse-34" class="active" style="display: block;">\n {completedString}</div>'''
-        oldData = oldData.replace('''</div><!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="index.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="icon" href="ViperWhite.ico">
-        <title>ViperVisionReleaseNotes</title>
-    </head>
-    <body>
-        <div class="main">
-            <div class="header">
-                <h1 id="title">ViperVision Release Notes</h1>\n''', "")
+        completedOutput = f'''<div id="{versionTitle}"><div class="align"><i id="i34" class="fa fa-minus" onclick="makeLittle('collapse-34', 'i34')" aria-hidden="true"></i><h2>ViperVision4.7.13.19</h2></div><ul id="collapse-34" class="active" style="display: block;"> {completedString}</ul></div>'''
         oldData = oldData.replace('''style="display: block;"''', "")
         oldData = oldData.replace("minus", "plus")
         
@@ -111,11 +87,31 @@ def stringParser(newTrelloCard):
         file = open("index.html", "w")
         file.write(finishedOutput)
         file.close
+        githubPush()
 
 def githubPush():
     g = Github("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")
-    pass
-
+    repo = g.get_user().get_repo("TestRepo")
+    all_files = []
+    contents = repo.get_contents("")
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents("index.html"))
+        else:
+            file = file_content
+            all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+    with open('index.html', 'r') as file:
+        content = file.read()
+    git_file = 'index.html'
+    if git_file in all_files:
+        contents = repo.get_contents(git_file)
+        repo.update_file(contents.path, "committing files", content, contents.sha, branch="main")
+        print(git_file + ' UPDATED')
+    else:
+        repo.create_file(git_file, "committing files", content, branch="main")
+        print(git_file + ' CREATED')
+githubPush()
 recurringLoop(getTrelloCard())
     
 
